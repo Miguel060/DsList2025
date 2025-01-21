2ef79a4bcbe3bbcfad3f75867a41823a2539d095
Miguel <migueldoiche2@gmail.com>
2025-01-20 23:36:34 -0300

    Project Created

---
+++ src/test/java/com/miguelsd/dslist2025/Dslist2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelsd.dslist2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dslist2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
